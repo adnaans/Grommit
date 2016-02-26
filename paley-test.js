07aa87609e65d7bede57c44cbff90f7ff996015d
Andrew Shi <ashilol95@gmail.com>
2016-02-26 15:34:26 -0800

    working on merge function

---
+++ paley-test.js
@@ -0,0 +1,28 @@
+function merge(x, y) {
+	var merged = [];
+	var i = 0;
+	var j = 0;
+	
+	while (i < x.length || j < y.length){
+		if (x[i] > y[j]) {
+			merged.push(x[i]);
+			i++;
+		} else {
+			merged.push(y[j]);
+			j++;
+		}
+	}
+	
+	return merged;
+}
+
+var x = [10, 10, 8, 7, 6, 5, 5, 4, 3, 2];
+var y = [11, 9, 9, 8, 7, 7, 6, 5, 3, 3, 3, 2, 1, 1];
+var c = merge(x, y);
+var s = "";
+
+for (var i = 0; i < c.length; i++){
+	s = s + c[i] + " ";
+}
+
+alert(s);
