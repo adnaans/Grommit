ae26b92be263817e93ca4dfabd9e615ea3ab430b
Adnaan Sachidanandan <adnaan15@gmail.com>
2016-04-18 15:43:22 -0700

    compatibility with multiple shapes

--- Legit Project/displaymap.js
+++ Legit Project/displaymap.js
@@ -1,30 +1,36 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 13,
     center: {lat: 37.444359, lng: -122.159902},
     mapTypeId: google.maps.MapTypeId.TERRAIN
   });
 
- // Define the LatLng coordinates for the polygon's path.
-  var data = [ //we need data for each section here
+  // Define the LatLng coordinates for the polygon's path.
+  var data = [[ //we need data for each section here
     {lat: 37.468876, lng: -122.155271},
     {lat: 37.449333, lng: -122.173628},
     {lat: 37.428431, lng: -122.139875},
     {lat: 37.448189, lng: -122.120452},
     {lat: 37.468876, lng: -122.155271}
+  ], [
+    {lat: 25.774, lng: -80.190},
+    {lat: 18.466, lng: -66.118},
+    {lat: 32.321, lng: -64.757},
+    {lat: 25.774, lng: -80.190}
+  ]
   ];
 
   //need to set color here. i will set the var for now
   var color = '#FF0000';
-
-  // for loop not working so we need to display each shape
-  var shape = new google.maps.Polygon({
-    paths: data,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: color,
-    fillOpacity: 0.35
-  });
-  shape.setMap(map);
+  for(var i = 0; i < data.length; i++){
+    var shape = new google.maps.Polygon({
+      paths: data[i],
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: color,
+      fillOpacity: 0.35
+    });
+    shape.setMap(map);
+  }
 }
