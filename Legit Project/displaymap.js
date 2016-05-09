c1535d4c37f3edc5775f96a7e557a94bd9b6b9fb
Adnaan Sachidanandan <adnaan15@gmail.com>
2016-05-09 16:02:15 -0700

    API call more efficient, color changes added

--- Legit Project/displaymap.js
+++ Legit Project/displaymap.js
@@ -19,45 +19,57 @@ function initMap() {
     {lat: 25.774, lng: -80.190}
   ]
   ];
+  var origin = {lat: 37.444359, lng: -122.159902};
+  var testdestination = {lat: 37.468876, lng: -122.155271};
+  var destinations = [testdestination];
 
-  //planning on making this more efficient with API calls when actual data comes in
-  //but for testing it will call the API once to get all the information  
-  for(var i = 0; i < data.length; i++){
-    var origin = {lat: 37.444359, lng: -122.159902};
+  console.log(destinations);
 
-    var destination = data[i]; //NEED TO DO CALCULATIONS TO GET CENTER
-    var duraton;
+  var matrix = new google.maps.DistanceMatrixService; //Making distance matrix
+  matrix.getDistanceMatrix({
+    origins: [origin],
+    destinations: destinations,
+    travelMode: google.maps.TravelMode.BICYCLING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+  }, function(response, status) { //upon completion
+      if (status == google.maps.DistanceMatrixStatus.OK) {
+        var originsx = response.originAddresses;
+        var destinationsx = response.destinationAddresses;
+        for (var i = 0; i < originsx.length; i++) {
+          var results = response.rows[i].elements;
+          for (var j = 0; j < results.length; j++) {
+            duration = results[j].duration.value; //Goes to location and stores value of seconds into duration variable
 
-    var matrix = new google.maps.DistanceMatrixService; //Making distance matrix
-    matrix.getDistanceMatrix({
-      origins: [origin],
-      destinations: [destination],
-      travelMode: google.maps.TravelMode.BIKING,
-      unitSystem: google.maps.UnitSystem.METRIC,
-    }, function(response, status) { //upon completion
-        if (status == google.maps.DistanceMatrixStatus.OK) {
-          var origins = response.originAddresses;
-          var destinations = response.destinationAddresses;
-          for (var i = 0; i < origins.length; i++) {
-            var results = response.rows[i].elements;
-            for (var j = 0; j < results.length; j++) {
-              duration = element.duration.value; //Goes to location and stores value of seconds into duration variable
+            //starting the grid
+
+            var color;
+ 
+            if(duration < 300){
+              color = "#FF0000";
             }
+            else if(duration < 600){
+              color = "#FF1A1A";
+            }
+            else if(duration < 900){
+              color = "#FF3333";
+            }
+            else{
+              color = "#FF4D4D";
+            }
+
+            var shape = new google.maps.Polygon({
+              paths: data[i],
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: color,
+              fillOpacity: 0.35
+            });
+            shape.setMap(map);
           }
         }
-      });
-
-    var color = "#FF0000"; //WE NEED TO DECIDE COLOR SCHEMES BASED ON CORE SECONDS HERE USING THE DURATION VARIABLE
-
-    var shape = new google.maps.Polygon({
-      paths: data[i],
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: color,
-      fillOpacity: 0.35
+      }
     });
-    shape.setMap(map);
-  }
+
 }
 
