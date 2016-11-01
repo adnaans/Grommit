981c8d38522cc1398b1ef4100b86633867f4fd4a
Adnaan Sachidanandan <adnaan15@gmail.com>
2016-11-01 15:50:04 -0700

    created new branch

--- Legit Project/displaymap.js
+++ Legit Project/displaymap.js
@@ -1,7 +1,12 @@
 var load = 0
+
 function initMap() {
   load++
   var select = document.getElementById("transportation");
+  var maxtime=0;
+  var mintime=0;
+  var times=new Array(80);
+  var destins=new Array(80);
 
   select.options[0] = new Option("Biking", 0);
   select.options[1] = new Option("Driving", 1);
@@ -88,9 +93,25 @@ function initMap() {
         methodtrans = google.maps.TravelMode.DRIVING;
         displaytext = "Driving";
       }
+      var destinstemp = destins;
+      var timestemp = times;
+      for (var j = 0; j < origins.length; j++){
+          var temp = calcTime(e.latLng, origins, j, shapes, displaytext, methodtrans, destinstemp, timestemp, mintime, maxtime);
+          destinstemp = temp[0];
+          timestemp = temp[1];
+          mintime = temp[2];
+          maxtime = temp[3];
+        }
+        destins = destinstemp;
+        times = timestemp;
       for (var i = 0; i < origins.length; i++){
-        calcTime(destination, origins, i, shapes, displaytext, methodtrans);
+        updateColors(i, displaytext, times, mintime, maxtime, shapes,destins);
       }
+      resetLegend(mintime, maxtime);
+      mintime=0;
+      maxtime=0;
+      times=new Array(80);
+      destins=new Array(80);
     }
     google.maps.event.addListener(map, "click", function(e){
       destination = e.latLng;
@@ -99,11 +120,25 @@ function initMap() {
       //marker.setMap(null);
       marker.position = destination;
       marker.setMap(map);
-
+     var destinstemp = destins;
+      var timestemp = times;
+      for (var j = 0; j < origins.length; j++){
+          var temp = calcTime(e.latLng, origins, j, shapes, displaytext, methodtrans, destinstemp, timestemp, mintime, maxtime);
+          destinstemp = temp[0];
+          timestemp = temp[1];
+          mintime = temp[2];
+          maxtime = temp[3];
+        }
+        destins = destinstemp;
+        times = timestemp;
       for (var i = 0; i < origins.length; i++){
-        calcTime(destination, origins, i, shapes, displaytext, methodtrans);
+        updateColors(i, displaytext, times, mintime, maxtime, shapes,destins);
       }
-
+      resetLegend(mintime, maxtime);
+      mintime=0;
+      maxtime=0;
+      times=new Array(80);
+      destins=new Array(80);
     });
 
     var colors = [];
@@ -159,10 +194,25 @@ function initMap() {
         //marker.setMap(null);
         marker.position = e.latLng;
         marker.setMap(map);
-
-        for (var j = 0; j < origins.length; j++){
-          calcTime(e.latLng, origins, j, shapes, displaytext, methodtrans);
+        var destinstemp = destins;
+      var timestemp = times;
+      for (var j = 0; j < origins.length; j++){
+          var temp = calcTime(e.latLng, origins, j, shapes, displaytext, methodtrans, destinstemp, timestemp, mintime, maxtime);
+          destinstemp = temp[0];
+          timestemp = temp[1];
+          mintime = temp[2];
+          maxtime = temp[3];
         }
+        destins = destinstemp;
+        times = timestemp;
+        for (var i = 0; i < origins.length; i++){
+        updateColors(i, displaytext, times, mintime, maxtime, shapes,destins);
+      }
+        resetLegend(mintime, maxtime);
+        mintime=0;
+        maxtime=0;
+        times=new Array(80);
+        destins=new Array(80);
       });
 
       var color;
@@ -207,12 +257,12 @@ function initMap() {
     }
   }
 }
-function calcTime(dest, ori, index, shapes, displaytext, methodtrans){
+function calcTime(dest, ori, index, shapes, methodtrans, destins, times, mintime, maxtime){
   var matrix = new google.maps.DistanceMatrixService;
   matrix.getDistanceMatrix({
     origins: [ori[index]],
     destinations: [dest],
-    travelMode: methodtrans,
+    travelMode: google.maps.TravelMode.BICYCLING,
     unitSystem: google.maps.UnitSystem.METRIC,
   }, function(response, status) { //upon completion
     if (status != google.maps.DistanceMatrixStatus.OK) {
@@ -221,27 +271,45 @@ function calcTime(dest, ori, index, shapes, displaytext, methodtrans){
     if (status == google.maps.DistanceMatrixStatus.OK) {
       var results = response.rows[0].elements;
       var destin = response.destinationAddresses[0];
+      destins[index]=destin;
       if (typeof(results[0].duration) == 'undefined'){
         time = -1
       } else {
         time = results[0].duration.value; //Goes to location and stores value of seconds into duration variable]
       }
-
+      times[index]=time;
+      if(maxtime==0)
+        maxtime = time;
+      else if(maxtime < time)
+        maxtime = time;
+      if(mintime > time)
+        mintime = time;
+  }});
+  return [destins, times, mintime, maxtime];
+  }
+function updateColors(index, displaytext, times, mintime, maxtime, shapes){
+      var time = times[index];
       if (time < 0){
         color = "#ababab"
       }
-      else if(time < 200 ){
+      else if(time < mintime+(maxtime-mintime)/4){
         color = "#ff0000";
       }
-      else if(time < 350){
+      else if(time < mintime+(maxtime-mintime)/4){
         color = "#66ffff";
       }
-      else if(time < 500){
+      else if(time < mintime+(maxtime-mintime)*3/4){
         color = "#66ff33";
       }
       else {
         color = "#3333cc";
       }
+      if(maxtime==0)
+        maxtime = time;
+      else if(maxtime < time)
+        maxtime = time;
+      if(mintime > time)
+        mintime = 0;
 
       shapes[index].setOptions({
         tempColor: color,
@@ -249,17 +317,28 @@ function calcTime(dest, ori, index, shapes, displaytext, methodtrans){
         fillColor: color
       });
       if (time >= 0){
-        shapes[index].window.setOptions({content: displaytext + " time from " + destin + ": " + results[0].duration.text});
+        shapes[index].window.setOptions({content: displaytext + " time from " + destins[index] + ": " + results[0].duration.text});
       } else {
         shapes[index].window.setOptions({content: "No" + displaytext + " route available"});
-      }
-    } else {
-      shapes[index].setOptions({
+        shapes[index].setOptions({
         tempColor: "#000000",
         strokeColor: "#000000",
         fillColor: "#000000"
       });
       shapes[index].window.setOptions({content: status});
+      }
     }
-  });
+  
+
+function resetLegend(mintime, maxtime){
+  var firstincrement, secondincrement, thirdincrement, fourthincrement;
+  var range = maxtime-mintime;
+  firstincrement = "" + mintime + " to " + range/4 + mintime + " minutes";
+  secondincrement = "" + mintime+ range/4 + " to " + mintime + range/2 + " minutes";
+  thirdincrement = "" + mintime + range/2 + " to " + mintime + range*3/4 + " minutes";
+  fourthincrement = "" + mintime + range*3/4 + " to " + mintime + maxtime + " minutes" ;
+  document.getElementById("firstc").textContent=firstincrement;
+  document.getElementById("secondc").textContent=secondincrement; 
+  document.getElementById("thirdc").textContent=thirdincrement; 
+  document.getElementById("fourthc").textContent=fourthincrement;  
 }
